3013521f2537f73c8494f79075c3837fd26d998b
Sahil Railkar <srailkar@box.com>
2021-04-27 15:01:39 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+// Uncomment the editor you would like to see
+
+// import Editor from './TipTap';
+// import Editor from './Draft';
+import Editor from './MySlate';
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
